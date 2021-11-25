0bc7249bd2e5457d75ea9fa45cf0ebf387a64eb9
Don Pardiola <30394723+dpardou@users.noreply.github.com>
2021-11-25 19:18:40 -0300

    Delete contacts/server directory

--- contacts/server/config/mongoose.config.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost/contacts")
-	.then(() => console.log("Conexión a la base de datos establecida correctamente"))
-	.catch(err => console.log("Ocurrió un problema al conectarse a la base de datos", err));
--- contacts/server/routes/user.route.js
+++
@@ -1,6 +0,0 @@
-const { register, login } = require('../controllers/user.controller');
-
-module.exports = app => {
-    app.post('/api/register', register);
-    app.post('/api/login', login);
-}
